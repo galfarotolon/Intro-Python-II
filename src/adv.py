9dcb30d7da30e83dd915d508195d1e0c7a764e6b
Guillermo Alfaro <galfarotolon@gmail.com>
2020-07-09 13:51:11 -0500

    refactored logic to clean up code for MVP day 1

--- src/adv.py
+++ src/adv.py
@@ -5,7 +5,7 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons."),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -18,8 +18,7 @@ the distance, but there is no way across the chasm."""),
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+chamber! The treasure piles up beyond sight, with glittering jewels and countless pieces of gold."""),
 }
 
 
@@ -60,6 +59,8 @@ player_name = input(f"\nHello adventurer! Please enter  your name to continue: "
 
 player = Player(player_name, room['outside'])
 
+possible_directions = ["n", "s", "e", "w"]
+
 print(player)
 
 
@@ -70,7 +71,7 @@ while True:
  
     # * Waits for user input and decides what to do.
 
-    player_input = input("\nWhich direction do you want to go next? Please enter 'n', 'e', 's' or 'w':  ").strip().lower().split()
+    player_input = input("\nWhich direction do you want to go next? Please enter 'n', 'e', 's' or 'w':  ").strip().lower().split()[0]
     player_input = player_input[0]
     
 # If the user enters "q", quit the game.
@@ -80,30 +81,46 @@ while True:
     
     # If the user enters a cardinal direction, attempt to move to the room there.
 
-    if player_input[0] == 'n':
 
+    if player_input in possible_directions:
+        # check to see if we can go in that direction 
+        # if we can, go there 
         try_direction(player, player_input)
-
-
-           # * Prints the current room name
-           # * Prints the current description (the textwrap module might be useful here).
         print(f"\nYou enter the {player.current_room.name}, {player.current_room.description}")
 
-    elif player_input[0] == 's':
-        try_direction(player, player_input)
-
-        print(f"\nYou enter the {player.current_room.name}, {player.current_room.description}")
-    elif player_input[0] == 'e':
-        try_direction(player, player_input)
-
-        print(f"\nYou enter the {player.current_room.name}, {player.current_room.description}")
-    elif player_input[0] == 'w':
-        try_direction(player, player_input)
-
-        print(f"\nYou enter the {player.current_room.name}, {player.current_room.description}")
-    
     else:
         print("\nThat is not a valid command. Please enter 'n', 'e', 's' or 'w' to keep playing, or 'q' to quit your adventure.")
 
-    
 
+
+        ## OLD CODE
+
+    #
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+    #
+    # If the user enters "q", quit the game.
+
+    # if player_input[0] == 'n':
+
+    #     try_direction(player, player_input)
+
+
+    #        # * Prints the current room name
+    #        # * Prints the current description (the textwrap module might be useful here).
+    #     print(f"\nYou enter the {player.current_room.name}, {player.current_room.description}")
+
+    # elif player_input[0] == 's':
+    #     try_direction(player, player_input)
+
+    #     print(f"\nYou enter the {player.current_room.name}, {player.current_room.description}")
+    # elif player_input[0] == 'e':
+    #     try_direction(player, player_input)
+
+    #     print(f"\nYou enter the {player.current_room.name}, {player.current_room.description}")
+    # elif player_input[0] == 'w':
+    #     try_direction(player, player_input)
+
+    #     print(f"\nYou enter the {player.current_room.name}, {player.current_room.description}")
+    
+  
